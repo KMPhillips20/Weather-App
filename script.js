37c396a60f2e2065f4a97a7c32f65bfe494804a0
Kyle <kylemphillips65@yahoo.com>
2023-01-31 15:23:55 -0500

    working on the api key and setting up fuctions in the script js file

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-
-var searchBtn = document.querySelector('#search-button');
-var weatherInput = document.querySelector('#weather');
-var toDay = document.querySelector("#toDay")
+var town = ""
+var apiKey = "0d014af5b06cb6bd943a8ea52f2c56e1"
 
 //  apiKey = "0d014af5b06cb6bd943a8ea52f2c56e1"
 //  url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`;
-// geo = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
-// weekUrl ="api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
-var apiKey = "0d014af5b06cb6bd943a8ea52f2c56e1";
+
+var findCity = function () {
+    var town = $("#townInput").val();
+    localStorage.setItem("town", town);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ town + "$appid=" + apiKey + "&units=imperial";
+    fetch(apiUrl)
+    .then(function (response){
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayInfo(data);
+            });
+        }
+    })
+}
 
