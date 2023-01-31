268af1daaf3c738d54382289262a6070c0b10b81
Kyle <kylemphillips65@yahoo.com>
2023-01-31 10:26:09 -0500

    added a search and a button so you can search for your city

--- script.js
+++ script.js
@@ -8,25 +8,5 @@ var toDay = document.querySelector("#toDay")
 // geo = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 // weekUrl ="api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
-var searchedCity = document.getElementById('#search');
 var apiKey = "0d014af5b06cb6bd943a8ea52f2c56e1";
 
-function fetchWeather(currentCity) {
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=5&appid=" + apiKey;
-
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-}
-var handleSearch = function (event) {
-        event.preventDefault();
-        var currentCity = searchedCity.value.trim();
-        fetchWeather(currentCity);
-}
